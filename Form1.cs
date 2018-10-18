8beaf3be687e2e7c33f0c7636d058c1ac4019072
user's_name <44219145+bdh1001@users.noreply.github.com>
2018-10-18 11:12:39 +0900

    The first version

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       // public string strSql = "Data Source=127.0.0.1:3306;Database=korea;User Id=root;Password=0000";
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            //sql 데이터베이스 이용 연동
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\meganext\Documents\Data1.mdf;Integrated Security=True;Connect Timeout=30"); //sql 데이터 베이스 연결 객체생성 
+            SqlDataAdapter sda = new SqlDataAdapter("Select Count(*) from USERINFO Where USERNAME='"+ID_txt.Text+"'and PASSWORD='"+PW_txt.Text+"'",con); //쿼리문과 C#을 같이
+            con.Open();
+            DataTable newtable = new DataTable();//userifo를 이용해 새로운 테이블
+            //아이디와 패스워드가 맞다면 newtable을 1로 반환 아니면 0으로 반환
+            sda.Fill(newtable);
+
+            if (newtable.Rows[0][0].ToString() == "1")//로그인이 성공할 경우
+            {
+                this.Hide();
+
+                MainForm mainForm = new MainForm();
+                mainForm.Show();
+            }
+            else//로그인 실패시
+            {
+                MessageBox.Show("아이디와 비밀번호를 확인해주세요");
+            }          
+            
+        }
+
+        private void JOIN_Click(object sender, EventArgs e)
+        {
+            Join join = new Join();
+            join.Show();
+        }
+    }
+}
---
+++ Join.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace LoginForm
+{
+    public partial class Join : Form
+    {
+        public Join()
+        {
+            InitializeComponent();
+        }
+
+        private Boolean idCheckd = false;
+        private Boolean pwCheckd = false;
+        Form1 form1 = new Form1();
+
+        private void pwCheck()
+        {
+            if (pwjoin_txt.Text == pwcheck_txt.Text)
+            {
+                pwCheckd = true;
+            }
+            else
+            {
+                pwCheckd = false;
+            }
+        }
+
+
+        private void idcheckbtn_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\meganext\Documents\Data1.mdf;Integrated Security=True;Connect Timeout=30");
+            con.Open();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = "Select * FROM USERINFO";
+            SqlDataReader sr = cmd.ExecuteReader();
+            
+            // var MConn = new MySqlConnection(form1.strSql);
+            // MConn.Open();
+            if (!idCheckd)
+            {
+
+               string sql = "select USERNAME, PASSWORD from USERINFO where USERNAME='" + idjoin_txt.Text + "'";
+
+               var Comm = new MySqlCommand(sql);
+               var myRead = Comm.ExecuteReader();
+                if(myRead.HasRows)
+                {
+                    idCheckd = false;
+                    label1.ForeColor = Color.Red;
+                    label1.Text = "아이디 중복";
+
+                }
+                else
+                {
+                    idCheckd = true;
+                    label1.ForeColor = Color.Blue;
+                    label1.Text = "OK확인";
+                }
+                con.Close();
+            }
+            
+
+        }
+
+        private void OK_click(object sender, EventArgs e)
+        {
+            pwCheck();
+            if (pwCheckd)
+            {
+                SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\meganext\Documents\Data1.mdf;Integrated Security=True;Connect Timeout=30");
+                con.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = "Select * FROM USERINFO";
+                SqlDataReader sr = cmd.ExecuteReader();
+                string sql = "insert into USERINFO(USERNAME, PASSWORD) values('" + idjoin_txt.Text + "','" + pwjoin_txt.Text + "')";
+                var Comm = new SqlCommand(sql);
+                int i = Comm.ExecuteNonQuery();
+                
+
+                //var MConn = new MySqlConnection(form1.strSql);
+                // MConn.Open();
+                // string sql = "insert into USERINFO(USERNAME, PASSWORD) values('" + idjoin_txt.Text + "','" + pwjoin_txt.Text + "')";
+                // var Comm = new MySqlCommand(sql, MConn);
+                //int i = Comm.ExecuteNonQuery();
+                //MConn.Close();
+
+                if (i==1)
+                {
+                    MessageBox.Show("가입완료", "확인", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+                else
+                {
+                    MessageBox.Show("비밀번호를 확인하세요", "확인", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                con.Close();
+            }
+            
+        }
+
+    }
+}
